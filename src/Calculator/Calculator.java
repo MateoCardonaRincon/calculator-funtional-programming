4e9c3e76da10584125f84ca0746ce0b9c730ab47
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-05-24 01:45:12 -0500

    Fixing error on multiply method

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -14,16 +14,18 @@ public class Calculator {
 
     private Integer multiply(Integer n1, Integer n2) {
         return (isZero.test(n1) || isZero.test(n2)) ? 0 : isPositive.test(n2) ?
-                sum.calculate(n1, multiply(n1, n2 - 1)) : subtract.calculate(multiply(n1, n2 + 1), n1);
+                sum.calculate(n1, multiply(n1, subtract.calculate(n2, 1))) :
+                subtract.calculate(multiply(n1, sum.calculate(n2, 1)), n1);
     }
 
     private Integer divide(Integer n1, Integer n2) {
         if (isZero.test(n2)) {
             throw new ArithmeticException("Error dividing by zero!");
         }
-        int signs = (Integer.signum(n1)) * (Integer.signum(n2));
-        return isZero.test(Math.abs(n1) - Math.abs(n2)) ?
-                signs : signs * sum.calculate(1, divide(subtract.calculate(Math.abs(n1), Math.abs(n2)), Math.abs(n2)));
+        int signs = multiply(Integer.signum(n1),Integer.signum(n2));
+        return isZero.test(subtract.calculate(Math.abs(n1), Math.abs(n2))) ?
+                signs :
+                multiply(signs, sum.calculate(1, divide(subtract.calculate(Math.abs(n1), Math.abs(n2)), Math.abs(n2))));
     }
 
     public void run() {
@@ -32,7 +34,8 @@ public class Calculator {
         System.out.println("7 - 9 = " + subtract.calculate(7, 9));
         System.out.println("3 * 5 = " + multiply(3, 5));
         System.out.println("-3 * 2 = " + multiply(-3, 2));
-        System.out.println("3 * (-2) = " + multiply(3, -2));
+        System.out.println("-1 * (-2) = " + multiply(-1, -2));
+        System.out.println("3 * (-4) = " + multiply(3, -4));
         System.out.println("8 / 4 = " + divide(8, 4));
         System.out.println("9 / (-3) = " + divide(9, -3));
         System.out.println("-10 / 2 = " + divide(-10, 2));
